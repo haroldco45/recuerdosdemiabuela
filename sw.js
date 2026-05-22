3632ceae92c5e6a45f83bb151efa22427e10c954
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-05-22 17:25:17 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
+const CACHE_NAME = 'recuerdos-abuela-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './recuerdos.PNG',
+  './kefir1.jpeg',
+  './kefir2.jpeg',
+  './kombucha.jpeg',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Crimson+Text:ital,wght@0,400;0,600;1,400&family=Dancing+Script:wght@600;700&display=swap'
+];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((res) => {
+        if (res && res.status === 200 && res.type !== 'opaque') {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
